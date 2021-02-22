eb81854b10f1dd36b2d30143dfb398b3709d6e10
PJ Winters <20544207+pjwin@users.noreply.github.com>
2021-02-22 17:27:19 -0500

    wrong guess country names now displayed in console

--- contentscript.js
+++ contentscript.js
@@ -1,35 +1,293 @@
 let userNick = document.getElementsByClassName("layout__header")[0].getElementsByClassName("user-nick")[0].innerHTML;
 console.log('Welcome %s', userNick);
 
-// const observer = new MutationObserver(function
-//   (mutations) {
-//   mutations.forEach(function (mutation) {
-//     if (mutation.addedNodes.length) {
-//       console.log(mutation.addedNodes[0])
-//     }
-//   })
-// });
+// Options for the observer (which mutations to observe)
+const documentObserverConfig = {
+  childList: true,
+  subtree: true
+};
 
-const wrongGuesses = document.getElementsByClassName("wrong-guesses__flags")[0];
-//.childNodes.length
-//const wrongGuessesSelect = document.querySelector("div.wrong-guesses__flags");
+// Callback function to execute when mutations are observed
+const documentObserverCallback = function (mutations) {
+  mutations.forEach(function (mutation) {
+    if (mutation.addedNodes.length) {
+      mutation.addedNodes.forEach(function (node) {
+        if (node.className === "game-state-overview__wrong-guesses") {
+          //get a NodeList of all the child nodes that are of class wrong-guesses__flag
+          var wrongFlags = node.querySelectorAll("div.wrong-guesses__flag");
+          wrongFlags.forEach(function (node) {
+              console.log(countries[node.getElementsByTagName("img")[0].alt.toUpperCase()]);
+          })
+        }
+        else if (node.className === "wrong-guesses__flag") {
+          console.log(countries[node.getElementsByTagName("img")[0].alt.toUpperCase()]);
+        }
+      })
+    }
+    if (mutation.removedNodes.length) {
 
-// observer.observe(wrongGuesses, {
-//   childList: true
-// })
+    }
+  })
+};
+
+// Create an observer instance linked to the callback function
+const documentObserver = new MutationObserver(documentObserverCallback);
+
+// Start observing the target node for configured mutations
+documentObserver.observe(document, documentObserverConfig);
 
 
+// document.contains(document.getElementsByClassName("wrong-guesses__flag")[0])
 
-// var myElement = $("<div>hello world</div>")[0];
-
-var observer = new MutationObserver(function (mutations) {
-  if (document.contains(wrongGuesses)) {
-    console.log("It's in the DOM!");
-    observer.disconnect();
-  }
-});
-
-observer.observe(document, { attributes: false, childList: true, characterData: false, subtree: true });
-
-
-document.contains(document.getElementsByClassName("wrong-guesses__flags")[0])
+countries = {
+	"AD": "Andorra",
+	"AE": "United Arab Emirates",
+	"AF": "Afghanistan",
+	"AG": "Antigua and Barbuda",
+	"AI": "Anguilla",
+	"AL": "Albania",
+	"AM": "Armenia",
+	"AO": "Angola",
+	"AQ": "Antarctica",
+	"AR": "Argentina",
+	"AS": "American Samoa",
+	"AT": "Austria",
+	"AU": "Australia",
+	"AW": "Aruba",
+	"AX": "Aland Islands",
+	"AZ": "Azerbaijan",
+	"BA": "Bosnia and Herzegovina",
+	"BB": "Barbados",
+	"BD": "Bangladesh",
+	"BE": "Belgium",
+	"BF": "Burkina Faso",
+	"BG": "Bulgaria",
+	"BH": "Bahrain",
+	"BI": "Burundi",
+	"BJ": "Benin",
+	"BL": "Saint Barthelemy",
+	"BM": "Bermuda",
+	"BN": "Brunei",
+	"BO": "Bolivia",
+	"BQ": "Bonaire, Saint Eustatius and Saba ",
+	"BR": "Brazil",
+	"BS": "Bahamas",
+	"BT": "Bhutan",
+	"BV": "Bouvet Island",
+	"BW": "Botswana",
+	"BY": "Belarus",
+	"BZ": "Belize",
+	"CA": "Canada",
+	"CC": "Cocos Islands",
+	"CD": "Democratic Republic of the Congo",
+	"CF": "Central African Republic",
+	"CG": "Republic of the Congo",
+	"CH": "Switzerland",
+	"CI": "Ivory Coast",
+	"CK": "Cook Islands",
+	"CL": "Chile",
+	"CM": "Cameroon",
+	"CN": "China",
+	"CO": "Colombia",
+	"CR": "Costa Rica",
+	"CU": "Cuba",
+	"CV": "Cape Verde",
+	"CW": "Curacao",
+	"CX": "Christmas Island",
+	"CY": "Cyprus",
+	"CZ": "Czech Republic",
+	"DE": "Germany",
+	"DJ": "Djibouti",
+	"DK": "Denmark",
+	"DM": "Dominica",
+	"DO": "Dominican Republic",
+	"DZ": "Algeria",
+	"EC": "Ecuador",
+	"EE": "Estonia",
+	"EG": "Egypt",
+	"EH": "Western Sahara",
+	"ER": "Eritrea",
+	"ES": "Spain",
+	"ET": "Ethiopia",
+	"FI": "Finland",
+	"FJ": "Fiji",
+	"FK": "Falkland Islands",
+	"FM": "Micronesia",
+	"FO": "Faroe Islands",
+	"FR": "France",
+	"GA": "Gabon",
+	"GB": "United Kingdom",
+	"GD": "Grenada",
+	"GE": "Georgia",
+	"GF": "French Guiana",
+	"GG": "Guernsey",
+	"GH": "Ghana",
+	"GI": "Gibraltar",
+	"GL": "Greenland",
+	"GM": "Gambia",
+	"GN": "Guinea",
+	"GP": "Guadeloupe",
+	"GQ": "Equatorial Guinea",
+	"GR": "Greece",
+	"GS": "South Georgia and the South Sandwich Islands",
+	"GT": "Guatemala",
+	"GU": "Guam",
+	"GW": "Guinea-Bissau",
+	"GY": "Guyana",
+	"HK": "Hong Kong",
+	"HM": "Heard Island and McDonald Islands",
+	"HN": "Honduras",
+	"HR": "Croatia",
+	"HT": "Haiti",
+	"HU": "Hungary",
+	"ID": "Indonesia",
+	"IE": "Ireland",
+	"IL": "Israel",
+	"IM": "Isle of Man",
+	"IN": "India",
+	"IO": "British Indian Ocean Territory",
+	"IQ": "Iraq",
+	"IR": "Iran",
+	"IS": "Iceland",
+	"IT": "Italy",
+	"JE": "Jersey",
+	"JM": "Jamaica",
+	"JO": "Jordan",
+	"JP": "Japan",
+	"KE": "Kenya",
+	"KG": "Kyrgyzstan",
+	"KH": "Cambodia",
+	"KI": "Kiribati",
+	"KM": "Comoros",
+	"KN": "Saint Kitts and Nevis",
+	"KP": "North Korea",
+	"KR": "South Korea",
+	"KW": "Kuwait",
+	"KY": "Cayman Islands",
+	"KZ": "Kazakhstan",
+	"LA": "Laos",
+	"LB": "Lebanon",
+	"LC": "Saint Lucia",
+	"LI": "Liechtenstein",
+	"LK": "Sri Lanka",
+	"LR": "Liberia",
+	"LS": "Lesotho",
+	"LT": "Lithuania",
+	"LU": "Luxembourg",
+	"LV": "Latvia",
+	"LY": "Libya",
+	"MA": "Morocco",
+	"MC": "Monaco",
+	"MD": "Moldova",
+	"ME": "Montenegro",
+	"MF": "Saint Martin",
+	"MG": "Madagascar",
+	"MH": "Marshall Islands",
+	"MK": "Macedonia",
+	"ML": "Mali",
+	"MM": "Myanmar",
+	"MN": "Mongolia",
+	"MO": "Macao",
+	"MP": "Northern Mariana Islands",
+	"MQ": "Martinique",
+	"MR": "Mauritania",
+	"MS": "Montserrat",
+	"MT": "Malta",
+	"MU": "Mauritius",
+	"MV": "Maldives",
+	"MW": "Malawi",
+	"MX": "Mexico",
+	"MY": "Malaysia",
+	"MZ": "Mozambique",
+	"NA": "Namibia",
+	"NC": "New Caledonia",
+	"NE": "Niger",
+	"NF": "Norfolk Island",
+	"NG": "Nigeria",
+	"NI": "Nicaragua",
+	"NL": "Netherlands",
+	"NO": "Norway",
+	"NP": "Nepal",
+	"NR": "Nauru",
+	"NU": "Niue",
+	"NZ": "New Zealand",
+	"OM": "Oman",
+	"PA": "Panama",
+	"PE": "Peru",
+	"PF": "French Polynesia",
+	"PG": "Papua New Guinea",
+	"PH": "Philippines",
+	"PK": "Pakistan",
+	"PL": "Poland",
+	"PM": "Saint Pierre and Miquelon",
+	"PN": "Pitcairn",
+	"PR": "Puerto Rico",
+	"PS": "Palestinian Territory",
+	"PT": "Portugal",
+	"PW": "Palau",
+	"PY": "Paraguay",
+	"QA": "Qatar",
+	"RE": "Reunion",
+	"RO": "Romania",
+	"RS": "Serbia",
+	"RU": "Russia",
+	"RW": "Rwanda",
+	"SA": "Saudi Arabia",
+	"SB": "Solomon Islands",
+	"SC": "Seychelles",
+	"SD": "Sudan",
+	"SE": "Sweden",
+	"SG": "Singapore",
+	"SH": "Saint Helena",
+	"SI": "Slovenia",
+	"SJ": "Svalbard and Jan Mayen",
+	"SK": "Slovakia",
+	"SL": "Sierra Leone",
+	"SM": "San Marino",
+	"SN": "Senegal",
+	"SO": "Somalia",
+	"SR": "Suriname",
+	"SS": "South Sudan",
+	"ST": "Sao Tome and Principe",
+	"SV": "El Salvador",
+	"SX": "Sint Maarten",
+	"SY": "Syria",
+	"SZ": "Swaziland",
+	"TC": "Turks and Caicos Islands",
+	"TD": "Chad",
+	"TF": "French Southern Territories",
+	"TG": "Togo",
+	"TH": "Thailand",
+	"TJ": "Tajikistan",
+	"TK": "Tokelau",
+	"TL": "East Timor",
+	"TM": "Turkmenistan",
+	"TN": "Tunisia",
+	"TO": "Tonga",
+	"TR": "Turkey",
+	"TT": "Trinidad and Tobago",
+	"TV": "Tuvalu",
+	"TW": "Taiwan",
+	"TZ": "Tanzania",
+	"UA": "Ukraine",
+	"UG": "Uganda",
+	"UM": "United States Minor Outlying Islands",
+	"US": "United States",
+	"UY": "Uruguay",
+	"UZ": "Uzbekistan",
+	"VA": "Vatican",
+	"VC": "Saint Vincent and the Grenadines",
+	"VE": "Venezuela",
+	"VG": "British Virgin Islands",
+	"VI": "U.S. Virgin Islands",
+	"VN": "Vietnam",
+	"VU": "Vanuatu",
+	"WF": "Wallis and Futuna",
+	"WS": "Samoa",
+	"XK": "Kosovo",
+	"YE": "Yemen",
+	"YT": "Mayotte",
+	"ZA": "South Africa",
+	"ZM": "Zambia",
+	"ZW": "Zimbabwe"
+}
